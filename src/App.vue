44167ac05c0c78b348073b32573b36b92dbc037c
ljp <1498725065@qq.com>
2020-05-27 10:37:23 +0800

    完成todoList的基本功能

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="my_ipt" v-model="inputValue" />
+    <a-button type="primary" @click="addTodo">添加事项</a-button>
+
+    <a-list bordered :dataSource="showList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox
+          :checked="item.done"
+          @change="
+            e => {
+              doneChange(e, item.id);
+            }
+          "
+        >{{ item.info }}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="delItemById(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDoneLength}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="listType === 'all' ? 'primary' : 'default'" @click="listChange('all')">全部</a-button>
+          <a-button :type="listType === 'undone' ? 'primary' : 'default'" @click="listChange('undone')">未完成</a-button>
+          <a-button  :type="listType === 'done' ? 'primary' : 'default'" @click="listChange('done')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clearAll">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {}
+  },
+  created() {
+    this.$store.dispatch('getdata')
+  },
+  computed: {
+    ...mapState(['listType']),
+    ...mapGetters(['unDoneLength', 'showList']),
+    inputValue: {
+      get() {
+        return this.$store.state.inputValue
+      },
+      set(value) {
+        this.$store.commit('inputValueChange', value)
+      }
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.error('The input Todo can not be null!')
+      }
+      this.$store.commit('addInputValue')
+      this.$message.success('Add new Todo can successfully!')
+    },
+    delItemById: function(id) {
+      this.$store.commit('deleteItem', id)
+    },
+    doneChange: function(e, id) {
+      this.$store.commit('doneChange', {
+        id: id,
+        value: e.target.checked
+      })
+    },
+    clearAll: function() {
+      this.$store.commit('clearAllDone')
+    },
+    listChange: function(type) {
+      this.$store.commit('listChange', type)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,67 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    dataList: [],
+    inputValue: 'do something',
+    nextId: 5,
+    listType: 'all'
   },
   mutations: {
+    initList(state, data) {
+      state.dataList = data
+    },
+    inputValueChange(state, value) {
+      state.inputValue = value
+    },
+    addInputValue(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.dataList.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    deleteItem(state, id) {
+      const itemId = state.dataList.findIndex(item => item.id === id)
+      if (itemId !== -1) state.dataList.splice(itemId, 1)
+    },
+    doneChange(state, params) {
+      const itemId = state.dataList.findIndex(item => item.id === params.id)
+      if (itemId !== -1) {
+        state.dataList[itemId].done = params.value
+      }
+    },
+    clearAllDone(state) {
+      state.dataList = state.dataList.filter(item => item.done === false)
+    },
+    listChange(state, type) {
+      state.listType = type
+    }
   },
   actions: {
+    getdata(context) {
+      axios.get('data.json').then(res => {
+        context.commit('initList', res.data)
+      })
+    }
   },
-  modules: {
-  }
+  getters: {
+    unDoneLength(state) {
+      return state.dataList.filter(item => item.done === false).length
+    },
+    showList(state) {
+      if (state.listType === 'all') return state.dataList
+      else if (state.listType === 'undone') return state.dataList.filter(item => !item.done)
+      else return state.dataList.filter(item => item.done)
+    }
+  },
+  modules: {}
 })
